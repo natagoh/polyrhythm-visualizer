3254b7b0a3ca028b56349d9633520c2940d6fcd8
Natsha Goh <gohnatasha98@gmail.com>
2021-09-11 23:58:38 -0400

    lcd algo

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  // euclidean algorithm
+  // requires x > y
+  const gcd = (x, y) => {
+    if (x === 0) return y;
+    if (y === 0) return x;
+    const remainder = x % y;
+    return gcd(y, remainder);
+  };
+
+  const lcd = (x, y) => (x * y) / (x > y
+    ? gcd(x, y) : gcd(y, x));
+
+  console.log(lcd(3, 2));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Polyrhythm Visualizer</h1>
-      </header>
+    <div className="container">
+      <h1>Polyrhythm Visualizer</h1>
     </div>
   );
 }
