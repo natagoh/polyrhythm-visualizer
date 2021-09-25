0c4b32ff1b7b6e35953bfcb0ffe10ea14de3bffe
Natsha Goh <gohnatasha98@gmail.com>
2021-09-25 02:20:58 -0400

    adding scroll listeners

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
@@ -9,7 +9,17 @@ import './Grid.css';
 export default function Grid(props) {
   const { left, right, animate } = props;
   const scrollRef = useSideScroll();
-  console.log('animate', animate);
+  const [scrolled, setScrolled] = useState(0);
+
+  // can find this defined in Grid.css in .rhythm-container div
+  const BEAT_WIDTH = 96;
+
+  useEffect(() => {
+    console.log('scrolling pos');
+    const rect = scrollRef.current.getBoundingClientRect();
+    const { width } = rect;
+    console.log('hi', rect, 'x', width);
+  }, []);
 
   // euclidean algorithm
   // requires x > y
@@ -27,49 +37,29 @@ export default function Grid(props) {
   const rightBeat = Math.floor(commonMultiple / right);
   const leftBeat = Math.floor(commonMultiple / left);
 
-  const beatsComponent = (
-    <>
-      <div className="rhythm-container">
-        {[...Array(commonMultiple).keys()].map((i) => {
-          const isBeat = i % rightBeat === 0;
-          return (
-            <div
-              className={cx({
-                note: !isBeat,
-                'note-beat': isBeat,
-                'note-right': isBeat,
-              })}
-              key={`note-right-${i}`}
-            >
-              {isBeat ? Math.floor(i / rightBeat) + 1 : null}
-            </div>
-          );
-        })}
-      </div>
-      <div className="rhythm-container">
-        {[...Array(commonMultiple).keys()].map((i) => {
-          const isBeat = i % leftBeat === 0;
-          return (
-            <div
-              className={cx({
-                note: !isBeat,
-                'note-beat': isBeat,
-                'note-left': isBeat,
-              })}
-              key={`note-left-${i}`}
-            >
-              {isBeat ? Math.floor(i / leftBeat) + 1 : null}
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
+  const handleScroll = (e) => {
+    const element = e.target;
+    if (element.scrollLeft >= BEAT_WIDTH) {
+      console.log('we need to add stuff');
+      if (animate) {
+        console.log('if animated add stuff');
+      }
+    }
 
-  const animatedBeatsComponent = (
-    <>
+    // reached the end of the scroll
+    // super useful diagram: https://stackoverflow.com/questions/21064101/understanding-offsetwidth-clientwidth-scrollwidth-and-height-respectively
+    if (element.scrollLeft + element.offsetWidth >= element.scrollWidth) {
+      console.log('we have reached the end of the line');
+    }
+    setScrolled(element.scrollLeft);
+
+    console.log('scoll element', scrolled);
+  };
+
+  const beatsComponent = (
+    <div>
       <div className="rhythm-container">
-        {[...Array(commonMultiple * 2).keys()].map((i) => {
+        {[...Array(commonMultiple).keys()].map((i) => {
           const isBeat = i % rightBeat === 0;
           return (
             <div
@@ -86,7 +76,7 @@ export default function Grid(props) {
         })}
       </div>
       <div className="rhythm-container">
-        {[...Array(commonMultiple * 2).keys()].map((i) => {
+        {[...Array(commonMultiple).keys()].map((i) => {
           const isBeat = i % leftBeat === 0;
           return (
             <div
@@ -102,12 +92,12 @@ export default function Grid(props) {
           );
         })}
       </div>
-    </>
+    </div>
   );
 
   return (
-    <div className="grid-container" ref={scrollRef}>
-      {animate ? animatedBeatsComponent : beatsComponent}
+    <div className="grid-container" ref={scrollRef} onScroll={handleScroll}>
+      {beatsComponent}
     </div>
   );
 }
